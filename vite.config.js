f2263f7a1043d7373c6ad05cda759e90afab042e
Vio <vio@beanon.com>
2023-01-09 20:07:44 +0100

    build: Output stats

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { webpackStats } from 'rollup-plugin-webpack-stats';
 
-export default {
-  plugins: [react()]
-}
+export default defineConfig((env) => {
+  console.log(env);
+
+  return {
+    plugins: [
+      react(),
+      webpackStats(),
+    ],
+  };
+});
