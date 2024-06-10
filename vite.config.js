0bba40e40a079811b20986652a54aa4a55749bc9
Viorel Cojocaru <vio@beanon.com>
2024-06-11 00:58:38 +0200

    build: Use common patterns for filenames

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 import { webpackStats } from 'rollup-plugin-webpack-stats';
 
 export default defineConfig((env) => ({
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
   plugins: [
     react(),
     // Output webpack-stats.json file
