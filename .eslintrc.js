968995e7dd0f603af08baa11c67283d18ce709d0
Viorel Cojocaru <vio@beanon.com>
2026-05-03 12:33:06 +0200

    build: Add tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['airbnb'],
+  overrides: [
+    {
+      files: ['vite.config.js'],
+      rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      },
+    },
+  ],
 };
